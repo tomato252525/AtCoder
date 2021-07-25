842b789401ddaf0c214613754dadbfd6b00ef1b4
YAMAMOTO <yamamoto@dunksoft.com>
2021-07-25 17:59:33 +0900

    2問目「CycleHit」を解いた

---
+++ Beginner211/Beginner211/CycleHit.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Beginner211
+{
+    class CycleHit
+    {
+        static void Main(string[] args)
+        {
+            string[] s = new string[4];
+            for (int i = 0; i < s.Length; i++)
+            {
+                s[i] = Console.ReadLine();
+            }
+            Console.WriteLine(s.Distinct().Count() == 4 ? "Yes" : "No");
+        }
+    }
+}
