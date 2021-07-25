ed7072f028023cb633b0fe2dd90a1d9a89427d7d
YAMAMOTO <yamamoto@dunksoft.com>
2021-07-25 17:57:43 +0900

    1問目「BloodPressure」を解いた

---
+++ Beginner211/Beginner211/BloodPressure.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Beginner211
+{
+    class BloodPressure
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+
+            double c = (double)(a - b) / 3 + b;
+
+            Console.WriteLine(c);
+        }
+    }
+}
