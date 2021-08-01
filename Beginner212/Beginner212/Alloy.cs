6f6559d1fb0267cd5c4f2e2a5f2384b53a350159
YAMAMOTO <yamamoto@dunksoft.com>
2021-08-01 15:21:46 +0900

    1問目「Alloy」を解いた

---
+++ Beginner212/Beginner212/Alloy.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Beginner212
+{
+    class Alloy
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+
+            int a = int.Parse(input[0]);
+            int b = int.Parse(input[1]);
+
+            string result;
+
+            if (0 < a && 0 < b)
+            {
+                result = "Alloy";
+            }
+            else
+            {
+                result = a < b ? "Silver" : "Gold";
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
