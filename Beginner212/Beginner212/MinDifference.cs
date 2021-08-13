46f2fc13e476b95282b509b2b11cd16cebd9901a
YAMAMOTO <yamamoto@dunksoft.com>
2021-08-13 15:25:50 +0900

    ああ

---
+++ Beginner212/Beginner212/MinDifference.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Beginner212
+{
+    class MinDifference
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine().Split(' ');
+
+            int n = int.Parse(input[0]);
+            int m = int.Parse(input[1]);
+            
+            var a = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            var b = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            Array.Sort(a);
+            Array.Sort(b);
+
+            int result = 0;
+            int ans;
+
+            for(int i=0;i<n;i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    ans = Math.Abs(a[i] - b[i]);
+                    if(ans > result)
+                    {
+                        break;
+                    }
+                    if(ans == 0)
+                    {
+                        Console.WriteLine(0);
+                        return;
+                    }
+                    result = ans;
+                }
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
---
+++ Beginner212/Beginner212/WeakPassword.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Beginner212
+{
+    class WeakPassword
+    {
+        static void Main(string[] args)
+        {
+            char[] x = Console.ReadLine().ToCharArray();
+            int weakFlg1 = 0, weakFlg2 = 0;
+
+            for (int i = 1; i < x.Length; i++)
+            {
+                int a = (int)Char.GetNumericValue(x[i - 1]);
+                int b = (int)Char.GetNumericValue(x[i]);
+                if (a == 9)
+                {
+                    a = -1;
+                }
+                if (x[i - 1] == x[i])
+                {
+                    weakFlg1++;
+                }
+                else if ((a + 1) == b)
+                {
+                    weakFlg2++;
+                }
+                else
+                {
+                    Console.WriteLine("Strong");
+                    return;
+                }
+                if (weakFlg1 != 0 && weakFlg2 != 0)
+                {
+                    Console.WriteLine("Strong");
+                    return;
+                }
+            }
+            Console.WriteLine("Weak");
+        }
+    }
+}
